e50b52e51da557482df5ecc9d0dda8eab8cc4af3
Grégory ALAVIN <70998888+gregory834@users.noreply.github.com>
2021-02-10 15:41:22 +0100

    Add files via upload

---
+++ moukatali3/script/script.js
@@ -0,0 +1,12 @@
+// OWL CAROUSEL
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        center: true,
+        items:1,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true
+    });
+  });
