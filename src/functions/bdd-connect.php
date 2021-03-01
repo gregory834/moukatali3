2b2ffcf0de1c0bd98a482099615768a78587dbec
Vincent <Fravette>
2021-03-01 08:21:47 +0400

    update bdd_connect.php

--- src/functions/bdd-connect.php
+++ src/functions/bdd-connect.php
@@ -9,16 +9,11 @@ function connectPdoBdd(){
         $pdo = new PDO('mysql:host=localhost;dbname=moukatali', $conn, $pass);
          $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
          $db='moukatali';
-<<<<<<< HEAD:functions/bdd-connect.php
-         echo 'METHOD PDO CONNECTION REUSSI AVEC FONCTION connectPdoBdd<br/>'; return $pdo;} 
-catch (PDOException $e) {print "Erreur !: " . $e->getMessage() . "<br/>";die();}}
-=======
          echo 'METHOD PDO CONNECTION REUSSI AVEC FONCTION connectPdoBdd<br/>';
         return $pdo;} 
     catch (PDOException $e) {print "Erreur !: " . $e->getMessage() . "<br/>";die();}}
 // connectPdoBdd();
 
->>>>>>> 0ac8ea26513238ea04282772432f5119bda49226:src/functions/bdd-connect.php
 ?>
 
 
