89db73a276fbdcc76f431af684da6d1218c1c8cb
David-HUMBLOT <humblot.david1@gamil.com>
2021-03-02 11:15:22 +0400

    ok

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ session_start();
 
 // définir des constantes globales
 define('ROOT_PATH', realpath(dirname(__FILE__, 2)));
-define('BASE_URL', 'http://127.0.0.1/edsa-moukatali');
+define('BASE_URL', 'http://127.0.0.1/edsa-moukatali3');
 
 // on appelle la classe Log
 require (ROOT_PATH . '/src/class/Log.class.php');
