ccceece7e6695f6d434df0bca462eb553b6bee14
Vincent <Fravette>
2021-03-02 09:01:06 +0400

    ajout fichier sql

--- db/moukatali-02-03.sql
+++ db/moukatali-02-03.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  jeu. 25 fév. 2021 à 11:20
+-- Généré le :  mar. 02 mars 2021 à 05:59
 -- Version du serveur :  5.7.17
 -- Version de PHP :  7.1.3
 
@@ -25,15 +25,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `like_dislike`
+-- Structure de la table `likes`
 --
 
-CREATE TABLE `like_dislike` (
+CREATE TABLE `likes` (
   `id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
   `moukatage_id` int(10) UNSIGNED NOT NULL,
   `action` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- --------------------------------------------------------
 
@@ -43,13 +43,20 @@ CREATE TABLE `like_dislike` (
 
 CREATE TABLE `moukatages` (
   `id` int(10) UNSIGNED NOT NULL,
-  `topics_id` int(10) UNSIGNED NOT NULL,
+  `topic_id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
-  `text` text NOT NULL,
+  `post` text NOT NULL,
   `likes` int(10) NOT NULL DEFAULT '0',
   `dislikes` int(10) NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `moukatages`
+--
+
+INSERT INTO `moukatages` (`id`, `topic_id`, `user_id`, `post`, `likes`, `dislikes`, `created_at`) VALUES
+(2, 1, 2, 'Ma premi&egrave;re publication', 0, 0, '2021-03-02 04:46:58');
 
 -- --------------------------------------------------------
 
@@ -61,11 +68,17 @@ CREATE TABLE `topics` (
   `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(100) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `quota_note` int(10) NOT NULL,
   `published` int(10) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `topics`
+--
+
+INSERT INTO `topics` (`id`, `title`, `image`, `published`, `created_at`, `updated_at`) VALUES
+(1, 'Premier sujet', 'premier-sujet.jpg', 1, '2021-03-02 04:44:06', NULL);
 
 -- --------------------------------------------------------
 
@@ -76,7 +89,7 @@ CREATE TABLE `topics` (
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
   `pseudo` varchar(255) NOT NULL,
-  `avatar` varchar(255) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -91,16 +104,17 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `pseudo`, `avatar`, `last_name`, `first_name`, `email`, `password`, `role`, `created_at`, `delete_account`) VALUES
-(1, 'admin', 'admin.jpg', 'Admin', 'Admin', 'admin@admin.com', '$2y$10$q9Qt/671Qtkopks.2csdheV9HzjSxCWI/jx8/.YpddBvRsy9ZFn9a', 'admin', '2021-02-25 10:19:16', 0);
+(1, 'admin', 'admin.jpg', 'Admin', 'Admin', 'admin@admin.com', '$2y$10$q9Qt/671Qtkopks.2csdheV9HzjSxCWI/jx8/.YpddBvRsy9ZFn9a', 'admin', '2021-02-25 10:19:16', 0),
+(2, 'user 1', '1614593975-avatar-1.jpg', 'User1', 'User1', 'user1@user.com', '$2y$10$zWG.NR1ATEjF/0DI3TZ6N.dWYZwvwgk6e1dd.MjUbyLOGEhc5sOLO', 'user', '2021-03-02 04:44:43', 0);
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `like_dislike`
+-- Index pour la table `likes`
 --
-ALTER TABLE `like_dislike`
+ALTER TABLE `likes`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `moukatage_id` (`moukatage_id`);
@@ -111,7 +125,7 @@ ALTER TABLE `like_dislike`
 ALTER TABLE `moukatages`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `user_id` (`user_id`),
-  ADD KEY `topics_id` (`topics_id`);
+  ADD KEY `topics_id` (`topic_id`);
 
 --
 -- Index pour la table `topics`
@@ -132,42 +146,42 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT pour la table `like_dislike`
+-- AUTO_INCREMENT pour la table `likes`
 --
-ALTER TABLE `like_dislike`
+ALTER TABLE `likes`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `moukatages`
 --
 ALTER TABLE `moukatages`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `topics`
 --
 ALTER TABLE `topics`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `like_dislike`
+-- Contraintes pour la table `likes`
 --
-ALTER TABLE `like_dislike`
-  ADD CONSTRAINT `like_dislike_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `like_dislike_ibfk_2` FOREIGN KEY (`moukatage_id`) REFERENCES `moukatages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`moukatage_id`) REFERENCES `moukatages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `moukatages`
 --
 ALTER TABLE `moukatages`
   ADD CONSTRAINT `moukatages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `moukatages_ibfk_2` FOREIGN KEY (`topics_id`) REFERENCES `topics` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `moukatages_ibfk_2` FOREIGN KEY (`topic_id`) REFERENCES `topics` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
